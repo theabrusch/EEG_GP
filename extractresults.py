dca016f72ce137ca39066c38a9a6c04d478e0e97
theb <s164238@student.dtu.dk>
2021-06-25 15:28:21 +0200

    extract results

---
+++ extractresults.py
@@ -0,0 +1,13 @@
+import pickle
+import numpy as np
+
+summary = pickle.load(open('outputs/training.pkl', 'rb'))
+
+accuracies_GP = np.zeros(len(summary.keys()))
+accuracies_LR = np.zeros(len(summary.keys()))
+bestsig = np.zeros(len(summary.keys()))
+
+for i in range(len(summary.keys())):
+    accuracies_GP[i] = summary[i]['accGP']
+    accuracies_LR[i] = summary[i]['accLR']
+    bestsig[i] = summary[i]['best_sigma']
