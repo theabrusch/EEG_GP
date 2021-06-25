6e22730fd127d12b8eb8e32e0f06fd2094655980
theb <s164238@student.dtu.dk>
2021-06-25 10:49:16 +0200

    stream line crossval

--- crossvalidation.py
+++ crossvalidation.py
@@ -8,89 +8,92 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import KFold
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import balanced_accuracy_score
 
-pickle_paths = glob.glob("features/emotion/*")
-df_collect = pd.DataFrame()
-labs = ['blink', 'lateyes', 'muscle', 'heart', 'gdcomps']
-for path in pickle_paths:
-    df_temp = pickle.load(open(path, 'rb'))
-    subj = path.split('/')[-1]
-    subjpath = 'SCCN_data/emotion/' + subj + '/sources.set'
-    file = loadmat(subjpath)['EEG'][0][0]
-    labels = np.zeros(len(df_temp))
-    i = 1
-    for lab in labs:
-        try:
-            labels[file[lab]] = i
-        except:
-            print('No', lab, 'comp for', subj)
-        i+=1
-    df_temp['label'] = labels.astype(np.int32)
-    df_collect = df_collect.append(df_temp)
-
+df_collect = pickle.load(open('features/data_collect.pkl', 'rb'))
 subjects = df_collect['subject'].unique()
 
 kfold = KFold(n_splits = 10)
 splits = kfold.split(subjects)
 #sigmas = np.arange(1, 3, step = 0.1)
-sigmas = [1.7]
+sigmas = [2,3,4,5,6,7,8,9,10]
 
 summary = dict()
 
 j = 0
 for (train,test) in splits:
+    print('Split', j+1, 'out of 10')
     train_subjects = subjects[train]
     test_subjects = subjects[test]
+
+    # Get train dataset
     df_train = df_collect[np.isin(df_collect['subject'], \
                                     train_subjects)].reset_index()
     X = df_train[df_collect.columns[:-2]].values
     y = df_train['label'].values
+
     # sample 50% of the mixed components
     mixed = df_train.index[df_train['label']==0]
     n_mixed = (df_train['label']==0).sum()
-    n_samp_mixed = int(n_mixed*0.3)
+    n_samp_mixed = int(n_mixed*0.5)
     mixed_sampled = np.random.choice(mixed, n_samp_mixed)
     not_mixed = df_train.index[y!=0]
     X_train = np.append(X[mixed_sampled,:], X[not_mixed,:], axis = 0)
     y_temp = np.append(y[mixed_sampled], y[not_mixed], axis = 0)
+
+    #transform y_train to format used by GP inference
     y_train = np.reshape(pd.get_dummies(y_temp).values,\
                         (6*len(y_temp),), order = 'F')
+    
+    #Get test dataset
     df_test = df_collect[np.isin(df_collect['subject'], test_subjects)]
     X_test = df_test[df_collect.columns[:-2]].values
     y_test = df_test['label'].values
 
-    X_stand = (X-np.mean(X, axis = 0)[np.newaxis,:])\
-               /np.std(X, axis = 0)[np.newaxis,:]
-    
-    X_test_stand = (X_test-np.mean(X, axis = 0)[np.newaxis,:])\
-                    /np.std(X_test, axis = 0)[np.newaxis,:]
+    # Standardize the train and test set
+    X_stand = (X_train-np.mean(X_train, axis = 0)[np.newaxis,:])\
+               /np.std(X_train, axis = 0)[np.newaxis,:]
+    X_test_stand = (X_test-np.mean(X_train, axis = 0)[np.newaxis,:])\
+                    /np.std(X_train, axis = 0)[np.newaxis,:]
     logliks = np.zeros(len(sigmas))
     i = 0
+    f_init = None
+    sol = []
+
+    # Loop over sigmas
     for sig in sigmas:
         SE = MultiClassKernel(num_classes = 6, params=[[sig,1]],\
                       base_kernel = SquaredExponentialKernel)
-        K = SE(X_train)
+        K = SE(X_stand)
         MC_GP = Multiclass_GP(SE, y_train, 6, K=K)
-        f, stats = MC_GP.inference(tol = 1e-6, maxiter = 50)
+        f, stats = MC_GP.inference(tol = 1e-6, maxiter = 20, f_init = f_init)
+        # initialize f at previous solution
+        f_init = MC_GP.f
         logliks[i] = stats['lml']
-    
+        sol.append(MC_GP)
+
+    #Get test results based on the kernel with maximum likelihood 
     max_loglik = np.argmax(logliks)
+    MC_GP = sol[max_loglik]
     sig = sigmas[max_loglik]
     SE = MultiClassKernel(num_classes = 6, params=[[sig,1]],\
                           base_kernel = SquaredExponentialKernel)
-    K = SE(X_train)
-    MC_GP = Multiclass_GP(SE, y_train, 6, K=K)
-    f, stats = MC_GP.inference(tol = 1e-6, maxiter = 100)
+    out = MC_GP.predict(X_test_stand, x = X_stand)
+    pred = np.argmax(out[0], axis = 1)
+
+    #Fit with Logistic regression
+    LR = LogisticRegression(multi_class= 'multinomial', class_weight='balanced', max_iter = 100)
+    LR.fit(X_stand, y_temp)
+    out_LR = LR.predict(X_test_stand)
+
     summary[j] = dict()
     summary[j]['test'] = test 
     summary[j]['best_sigma'] = sig
     summary[j]['y_test'] = y_test
-    out = MC_GP.predict(X_test, x = X_stand)
-    pred = np.argmax(out[0], axis = 1)
     summary[j]['pred'] = pred
     summary[j]['dist'] = out[1]
+    summary[j]['predLR'] = out_LR
     j+=1
 
-
-plt.matshow(K)
-plt.show()
+pickle.dump(summary, open('outputs/training.pkl', 'wb'))
